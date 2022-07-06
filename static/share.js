b58f178d410a85103c99e13fabec58483fef157b
Andrea <bort0x@tuta.io>
2022-07-06 12:03:57 +0200

    Evoco il cane(...) per le virgole

--- static/share.js
+++ static/share.js
@@ -6,11 +6,11 @@ function condividipagina(titolo, descrizione, permalink, testocondividi) {
     document.body.appendChild(share_script);
     share_script.addEventListener('load', function() {
         var n = document.createElement("share-menu");
-        n.setAttribute("title",titolo)
-        n.setAttribute("text", descrizione)
-        n.setAttribute("url", permalink)
-        n.setAttribute("dialog-title", testocondividi)
-        n.setAttribute("id", shareMenu)
+        n.setAttribute("title",titolo);
+        n.setAttribute("text", descrizione);
+        n.setAttribute("url", permalink);
+        n.setAttribute("dialog-title", testocondividi);
+        n.setAttribute("id", "shareMenu");
         document.body.appendChild(n);
         shareMenu.share();
         _paq.push(['trackEvent', 'Share', 'Share webpage', '{{ .Title }}']);
